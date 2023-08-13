75263e225e8203d7999db0f41af80f6b8e62dfb7
niuheshui <niuheshui@gmail.com>
2023-08-13 19:42:51 +0800

    update install

--- apt/install.sh
+++ apt/install.sh
@@ -1,4 +1,5 @@
 vim
+htop
 git
 tldr
 tmux
--- color_256.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-def get(i):
-	s = '\033[48;5;%dm      \033[0m %3d' % (i, i)	
-	return s	
-
-for i in range(64):
-	print(get(i), get(64 + i), get(128 + i), get(192 + i))
