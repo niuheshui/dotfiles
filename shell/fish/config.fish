4aa4100c9be05ac5746ef6b4f6ea7d12aebbbf54
niuheshui <niuheshui@gmail.com>
2024-02-10 21:31:50 +0800

    auto commit

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -6,6 +6,10 @@ if test ~/.aliases
     source ~/.aliases
 end
 
+if test ~/.environment
+    source ~/.environment
+end
+
 function fish_greeting
     date
     echo "hello ($USER@$hostname)"
@@ -13,20 +17,3 @@ function fish_greeting
 end
 
 fish_add_path -aP $HOME/.local/bin
-
-export no_proxy=localhost,127.0.0.1,::1
-export NO_PROXY=localhost,127.0.0.1,::1
-
-export all_proxy=socks://127.0.0.1:7890/
-export ALL_PROXY=socks://127.0.0.1:7890/
-
-export http_proxy=http://127.0.0.1:7890/
-export HTTP_PROXY=http://127.0.0.1:7890/
-
-export https_proxy=http://127.0.0.1:7890/
-export HTTPS_PROXY=http://127.0.0.1:7890/
-
-
-export NEMU_HOME=$HOME/ics2020/nemu
-export AM_HOME=$HOME/ics2020/abstract-machine
-export REPO_DIR=$HOME/CS61B
