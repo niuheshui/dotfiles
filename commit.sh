54ba90b783b1645918cd3b20ce3ba869b0cd4df0
niuheshui <niuheshui@gmail.com>
2024-02-27 18:58:17 +0800

    M  commit.sh

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@ tmpfile=/tmp/git-commit-message.txt
 remote=origin
 remote_branch=main
 
-git add . && \ 
-git commit --short > $tmpfile && \
-git commit -F $tmpfile && \
+git add . 
+git commit --short > $tmpfile 
+git commit -F $tmpfile 
 git push $remote $remote_branch
