271d29c3485f0b97ce85b170351486a06a42c3d7
niuheshui <niuheshui@gmail.com>
2024-02-27 18:56:04 +0800

    M  commit.sh M  vim/nvim/lua/core/plugins_config/treesitter.lua

--- commit.sh
+++ commit.sh
@@ -1,2 +1,8 @@
 tmpfile=/tmp/git-commit-message.txt
-git add . && git commit --short > $tmpfile && git commit -F $tmpfile
+remote=origin
+remote_branch=main
+
+git add . && \ 
+git commit --short > $tmpfile && \
+git commit -F $tmpfile && \
+git push $remote $remote_branch
--- vim/nvim/lua/core/plugins_config/treesitter.lua
+++ vim/nvim/lua/core/plugins_config/treesitter.lua
@@ -1,6 +1,12 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "java", "bash", "python", "json" },
+  ensure_installed = {
+    "c", "lua", "vim",
+    "vimdoc", "query",
+    "java", "bash",
+    "python", "json",
+    "make",
+  },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
