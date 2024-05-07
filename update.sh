9ca9cfbc60f2ee7e1f4b7a8e9e4b0962752951e8
niuheshui <niuheshui@gmail.com>
2024-05-07 17:19:22 +0800

    M  shell/fish/fish_variables A  update.sh

---
+++ update.sh
@@ -0,0 +1,14 @@
+remote=origin
+
+if [[ $# -gt 0 ]]
+then
+    remote=$1
+fi
+
+# branch=$(git branch | grep "\*" | sed "s/\*//" | xargs echo)
+# branch=$(git branch | grep "\*" | cut -d ' ' -f2)
+branch=$(git branch | grep '\*' | awk  '{ print $2 }')
+
+echo "git pull ${remote} ${branch}"
+
+git pull ${remote} ${branch}
