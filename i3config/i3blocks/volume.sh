4ba7fa6a1a6fb7ebb7078491bc818d0f18884a5a
niuheshui <niuheshui@gmail.com>
2024-06-26 12:41:52 +0800

    M  fcitx5/conf/cached_layouts M  fcitx5/conf/chttrans.conf M  fcitx5/conf/notifications.conf M  fcitx5/conf/pinyin.conf M  fcitx5/conf/punctuation.conf M  i3config/i3blocks/config M  i3config/i3blocks/volume.sh M  shell/fish/fish_variables M  vim/nvim/plugin/packer_compiled.lua

--- i3config/i3blocks/volume.sh
+++ i3config/i3blocks/volume.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-msg=$(echo 󰕾 $(amixer -c 1 get Master | grep -o -E '([0-9]*%|\[(off|on)\])' | sort | uniq))
+msg=$(echo 󰕾 $(amixer get Master | grep -o -E '([0-9]*%|\[(off|on)\])' | sort | uniq))
 notify-send "$msg"
 echo $msg
 
