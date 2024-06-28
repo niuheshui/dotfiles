07c9dd10cad549d7d2e54b5ae35b7ee8bee2aeda
niuheshui <niuheshui@gmail.com>
2024-06-28 21:46:07 +0800

    M  fcitx5/conf/cached_layouts M  fcitx5/conf/chttrans.conf M  fcitx5/conf/notifications.conf M  fcitx5/conf/pinyin.conf M  fcitx5/conf/punctuation.conf M  fcitx5/config M  scripts/date.sh

--- scripts/date.sh
+++ scripts/date.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 # 保存当前终端设置
 old_stty_cfg=$(stty -g)
 
@@ -16,11 +17,13 @@ while true; do
   if read -t 0.1 -n 1 key; then
     case "$key" in
       "q") echo -e '\x1b[2J\x1b[H'; break ;;
+      "l") echo -e '\x1b[2J' ;;
     esac
+
   else
     echo -e '\x1b[H'
-    #date +%T | toilet -f mono12 --filter gay
-    date +%T | toilet -f mono12 
+    # date +%T | toilet -f mono12 --gay -F border
+    date +%T | toilet -f mono12 --gay -F crop
   fi
 
 done
